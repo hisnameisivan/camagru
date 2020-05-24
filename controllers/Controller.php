8f64c13c41dacd6aa42030298957bb5ee7551db3
Ivan Guzeev <hisnameisivan@ya.ru>
2020-05-24 17:11:44 +0300

    controllers/Controller.php - add view

--- controllers/Controller.php
+++ controllers/Controller.php
@@ -2,13 +2,17 @@
 
 namespace controllers;
 
+use view\View;
+
 abstract class Controller
 {
 	public $route;
+	public $view;
 
 	function __construct($route)
 	{
 		$this->route = $route;
+		$this->view = new View($route);
 	}
 
 }
