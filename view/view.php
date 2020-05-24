9247106a117380f2f75281c8643340cb8b024f7b
Ivan Guzeev <hisnameisivan@ya.ru>
2020-05-24 17:15:41 +0300

    view/view.php - add render(), update contructor

--- view/view.php
+++ view/view.php
@@ -2,14 +2,33 @@
 
 namespace view;
 
-abstract class View
+class View
 {
 	public $path;
+	public $route;
 	public $layout = 'default';
 
 	function __construct($route)
 	{
 		$this->route = $route;
+		$this->path = $route['controller'].'/'.$route['action'];
+		// print_r($this->path);
 	}
 
+	function render($title, $vars = [])
+	{
+		if (file_exists('view/'.$this->path.'.php')) {
+			ob_start();
+			require_once('view/'.$this->path.'.php');
+			$content = ob_get_clean();
+
+			if (file_exists('view/layouts/'.$this->layout.'.php')) {
+				require_once('view/layouts/'.$this->layout.'.php');
+			} else {
+				echo 'Error requiring file \'view/layouts/'.$this->layout.'.php\'';
+			}
+		} else {
+			echo 'Error requiring file \'view/'.$this->path.'.php\'';
+		}
+	}
 }
