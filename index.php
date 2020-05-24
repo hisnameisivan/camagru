cdf95df1c842aaa9f6d956293923ed38c2a1200d
Ivan Guzeev <hisnameisivan@ya.ru>
2020-05-24 17:13:43 +0300

    index.php - add view

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 require_once('dev.php');			// all errors, debug
 require_once('router/router.php');		// class router
 // порядок require играет роль
+require_once('view/view.php');
 require_once('controllers/Controller.php');
 require_once('controllers/AccountController.php');
 require_once('controllers/MainController.php');
