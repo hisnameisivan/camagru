7f94111364c466b6c609a3cf240687ff31d3f91a
Ivan Guzeev <hisnameisivan@ya.ru>
2020-06-07 00:32:12 +0300

    index.php - add require

--- index.php
+++ index.php
@@ -4,6 +4,9 @@ require_once('dev.php');			// all errors, debug
 require_once('router/router.php');		// class router
 // порядок require играет роль
 require_once('view/view.php');
+require_once('db/db.php');
+require_once('models/Model.php');
+require_once('models/MainModel.php');
 require_once('controllers/Controller.php');
 require_once('controllers/AccountController.php');
 require_once('controllers/MainController.php');
