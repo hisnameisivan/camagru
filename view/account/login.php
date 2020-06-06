ccddf26a9406a9cdff51c8f3fb7aa98e8fb3c0cd
Ivan Guzeev <hisnameisivan@ya.ru>
2020-06-07 01:12:28 +0300

    view/account/login.php - jQuery test

--- view/account/login.php
+++ view/account/login.php
@@ -1,8 +1,12 @@
 <h3>Вход</h3>
-<form>
+<form action="/account/login" method="post">
+<!-- <form> -->
 	<p>Логин</p>
-	<p><input type="text"></p>
+	<p><input type="text" name="login"></p>
+	<!-- <p><input type="text"></p> -->
 	<p>Пароль</p>
-	<p><input type="text"></p>
-	<b><button>Вход</button></b>
+	<p><input type="text" name="password"></p>
+	<!-- <p><input type="text"></p> -->
+	<b><button type="submit" name="enter">Вход</button></b>
+	<!-- <b><button>Вход</button></b> -->
 </form>
