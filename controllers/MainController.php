49402f4a9c313ebda41eb77a3d8c191cd18382ed
Ivan Guzeev <hisnameisivan@ya.ru>
2020-06-07 00:58:44 +0300

    controllers/MainController.php - Model test

--- controllers/MainController.php
+++ controllers/MainController.php
@@ -2,11 +2,22 @@
 
 namespace controllers;
 
+use db\Db;
+
 class MainController extends Controller
 {
 	function indexAction()
 	{
+		// $result = $this->model->getNews();
+		// $vars = [
+		// 	'news' => $result,
+		// ];
+
+		// $this->view->render('Главная страница', $vars);
 		$this->view->render('Главная страница');
+		// уходит в model
+		// $db = new Db;
+		// $db->query('select * from test_table');
 		// echo 'Главная страница';
 	}
 
