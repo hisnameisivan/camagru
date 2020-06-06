5a5086d00d852f2a591949d8e1296b89514cbf2a
Ivan Guzeev <hisnameisivan@ya.ru>
2020-06-07 01:03:37 +0300

    db/db.php - Db - class for working with database

---
+++ db/db.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace db;
+
+use PDO;
+
+class Db
+{
+
+	protected $db;
+
+	function __construct()
+	{
+		$configDBPath = 'config/db.php';
+		if (file_exists($configDBPath)) {
+			$config = require_once($configDBPath);
+			try {
+				$this->db = new PDO('mysql:host='.$config['host'].';dbname='.$config['name'], $config['user'], $config['password']);
+			} catch (PDOException $e) {
+				die('Connection failed: ' . $e->getMessage());
+			}
+		}
+	}
+
+	// function query($sql)
+	// {
+	// 	$this->db->query($sql);
+	// }
+}
