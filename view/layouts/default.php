e1c6708b12a65c814fd79943bb8331571ac561c1
Ivan Guzeev <hisnameisivan@ya.ru>
2020-06-26 04:06:44 +0300

    view/layouts/default.php - add footer, font logo

--- view/layouts/default.php
+++ view/layouts/default.php
@@ -4,40 +4,32 @@
     <title><?php echo $title; ?></title>
     <!-- <script src="/scripts/jquery.js"></script>
     <script src="/scripts/form.js"></script> -->
-    <!-- <style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        -ms-user-select: none;
-        user-select: none;
-      }
 
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
-      }
-    </style> -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <!-- <link rel="stylesheet" href="../../css/styles.css"> -->
+    <link rel="stylesheet" href="../../css/styles.css">
+    <link href="https://fonts.googleapis.com/css2?family=Pangolin&display=swap" rel="stylesheet">
+    <link rel="shortcut icon" type="image/png" href="/icon/favicon-16x16.png">
   </head>
   <body>
+
     <header class="d-flex justify-content-between align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
       <div id=logo>
-        <!-- TODO camagru font -->
-        <a href="/">Camagru</a>
+        <a id="logo" href="/">Camagru</a>
       </div>
       <div class="d-inline">
-        <a class="btn btn-outline-primary" href="/account/login">Log In</a>
+        <a class="btn btn-outline-primary" href="/account/login">Log in</a>
         <a class="btn btn-outline-primary" href="/account/register">Sign up</a>
       </div>
     </header>
-    <main>
+
+    <main class="content">
+      <?php echo $content; ?>
     </main>
-    <footer>
+
+    <footer class="d-flex justify-content-center align-items-center pt-4 my-md-5 pt-md-5 border-top">
+      <div id="about">
+        2020, <a href="https://profile.intra.42.fr/users/waddam">Ivan Guzeev</a>
+      </div>
     </footer>
-    <?php echo $content; ?>
   </body>
 </html>
