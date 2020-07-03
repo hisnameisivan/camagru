f99d9d5845225caf97bd483309deb4ba23a8817e
Ivan Guzeev <hisnameisivan@ya.ru>
2020-07-03 03:37:40 +0300

    view/account/* - views for sign up and log in

--- view/account/login.php
+++ view/account/login.php
@@ -1,12 +1,19 @@
-<h3>Вход</h3>
+<!-- <h3>Вход</h3>
 <form action="/account/login" method="post">
-<!-- <form> -->
     <p>Логин</p>
     <p><input type="text" name="login"></p>
-    <!-- <p><input type="text"></p> -->
     <p>Пароль</p>
     <p><input type="text" name="password"></p>
-    <!-- <p><input type="text"></p> -->
     <b><button type="submit" name="enter">Вход</button></b>
-    <!-- <b><button>Вход</button></b> -->
-</form>
+</form> -->
+<div class="text-center">
+  <form class="form-signin">
+    <!-- <div id=logo>Camagru</div> -->
+    <h3 class="account">Sign in to see photos and videos from your friends</h3>
+    <!-- <label for="inputEmail" class="sr-only">Email address</label> -->
+    <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+    <!-- <label for="inputPassword" class="sr-only">Password</label> -->
+    <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+    <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+  </form>
+</div>
--- view/account/register.php
+++ view/account/register.php
@@ -1,8 +1,23 @@
-<h3>Регистрация</h3>
-<form>
+<!-- <h3>Регистрация</h3>
+<form action="/account/register" method="post">
     <p>Логин</p>
-    <p><input type="text"></p>
+    <p><input type="text" name="login"></p>
     <p>Пароль</p>
-    <p><input type="text"></p>
-    <b><button>Регистрация</button></b>
-</form>
+    <p><input type="text" name="password"></p>
+    <b><button type="submit" name="enter">Регистрация</button></b>
+</form> -->
+<div class="text-center">
+  <form class="form-signin">
+    <!-- <div id=logo>Camagru</div> -->
+    <h3 class="account">Sign up to see photos and videos from your friends</h3>
+    <!-- <label for="inputEmail" class="sr-only">Email address</label> -->
+    <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+    <!-- <label for="inputEmail" class="sr-only">Email address</label> -->
+    <input type="text" id="inputNickname" class="form-control" placeholder="Nickname" required>
+    <!-- <label for="inputPassword" class="sr-only">Password</label> -->
+    <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+    <!-- <label for="inputPasswordRep" class="sr-only">Password</label> -->
+    <input type="password" id="inputPasswordRep" class="form-control" placeholder="Repeat password" required>
+    <button class="btn btn-lg btn-primary btn-block" type="submit">Sign up</button>
+  </form>
+</div>
